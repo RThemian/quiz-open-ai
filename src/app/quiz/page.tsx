063e4986e839522d709d60c2da45f581d3002431
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2024-02-03 09:06:36 -0700

    prisma schema pushed to db

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,14 @@
+// boilerplate code for quiz page
+import React from 'react';
+
+type Props = {}; // add props here explain: https://reactjs.org/docs/components-and-props.html
+
+const Quiz = (props: Props) => {
+    return (
+        <div>
+        <h1>Quiz</h1>
+        </div>
+    );
+    };
+
+export default Quiz;
