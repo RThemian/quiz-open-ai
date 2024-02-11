8cac145f78b2c37193ca644affe9aabe4bfc5cfb
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2024-02-11 13:34:51 -0700

    adds home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/db";
+//make server component with async
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-export default function Home() {
+
+export default async function Home() {
   return (
-    <Button variant="destructive">Hello World</Button>
-  );
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w- [300px]">
+        <CardHeader>
+          <CardTitle>
+            Welcome to Open AI Quiz
+          </CardTitle>
+          <CardDescription>
+            Create and Share Quizzes from AI
+          </CardDescription>
+        </CardHeader>
+        
+      </Card>
+    </div>
+    );
 }
